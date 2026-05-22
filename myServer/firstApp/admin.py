4fe1cddab0f266cb7267041c0ac025fb33b532cb
Ariyan Amiri <219530074+AriyanAmiri01@users.noreply.github.com>
2026-05-22 15:06:08 +0200

    Model Registration Completed

--- myServer/firstApp/admin.py
+++ myServer/firstApp/admin.py
@@ -1,8 +1,24 @@
+"""
+@file admin.py
+@brief Django admin panel configuration.
+
+@details
+Registers ORM models so they can be managed through
+the Django admin interface.
+
+@author Ariyan Amiri
+@version 1.0
+@date 2026-05-22
+
+@see https://github.com/AriyanAmiri01/Web_Playground
+"""
+
+# Django admin utilities
 from django.contrib import admin
 
-# Register your models here.
-from django.contrib import admin
+# Application models
 from .models import Tag, Project
 
+# Register models with the admin panel
 admin.site.register(Tag)
 admin.site.register(Project)

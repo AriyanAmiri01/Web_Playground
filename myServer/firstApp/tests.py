7ab2c54e4cd30131ca367b0ea92e661f892b3728
Ariyan Amiri <219530074+AriyanAmiri01@users.noreply.github.com>
2026-05-22 15:22:55 +0200

    tests.py file commented

--- myServer/firstApp/tests.py
+++ myServer/firstApp/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+"""
+@file tests.py
+@brief I did not use this at all
 
-# Create your tests here.
+
+@author Ariyan Amiri
+@version 1.0
+@date 2026-05-22
+
+@see https://github.com/AriyanAmiri01/Web_Playground
+"""
+from django.test import TestCase
